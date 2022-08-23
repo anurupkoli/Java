0e6d41dd6eb9540676cac0366ed34ce3f220e2fb
Anurup <anurupkoli01@gmail.com>
2022-08-23 17:56:51 +0530

    new

--- PracticeSet04.java/Set4_pr_04.java
+++ PracticeSet04.java/Set4_pr_04.java
@@ -5,26 +5,14 @@ public class Set4_pr_04 {
         System.out.print("Enter the day: ");
         int day = s.nextInt();
 
-        if(day == 1){
-            System.out.println("Monday");
-        }
-        else if(day == 2){
-            System.out.println("Tuesday");
-        }
-        else if(day == 3){
-            System.out.println("Wednesday");
-        }
-        else if(day == 4){
-            System.out.println("Thursday");
-        }
-        else if(day == 5){
-            System.out.println("Friday");
-        }
-        else if(day == 6){
-            System.out.println("Saturday");
-        }
-        else if(day == 7){
-            System.out.println("Sunday");
+        switch(day){
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
         }
     }    
 	
