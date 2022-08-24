35765ee0d23505b0d3dabeb9cb8fd60d145bf13f
Anurup <anurupkoli01@gmail.com>
2022-08-25 00:43:37 +0530

    new

--- PracticeSet06.java/Set6_Pr_03.java
+++ PracticeSet06.java/Set6_Pr_03.java
@@ -1,8 +1,8 @@
 public class Set6_Pr_03 {
     public static void main(String[] args) {
-       int []array = {99,56,87,63,54,75,88,87,78,72,99,100,33,65,75,87};
-       int totalMarks = 0;
-       for(int elements: array){
+       float []array = {99,56,87,63,54,75,88,87,78,72,99,100,33,65,75,87};
+       float totalMarks = 0;
+       for(float elements: array){
         totalMarks += elements;
        }
        System.out.println(totalMarks/array.length + "%"); 
