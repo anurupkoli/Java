29068c469301a3337672e19b2badb225f7c71330
Anurup <anurupkoli01@gmail.com>
2022-08-25 00:08:36 +0530

    dff

--- PracticeSet06.java/Set6_Pr_01.java
+++ PracticeSet06.java/Set6_Pr_01.java
@@ -2,8 +2,8 @@ public class Set6_Pr_01 {
     public static void main(String[] args){
         float [] array = {5.22f, 9.66f, 7.82f, 8.62f, 2.64f};
         float sum = 0;
-        for(int i=0; i<array.length; i++){
-            sum += array[i];
+        for(float elements : array){
+            sum += elements;
         }
         System.out.printf("Sum is: %.2f", sum);
     }
