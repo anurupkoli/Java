f76058bdceb9a578d32984a789a2c79674e91ae4
Anurup <anurupkoli01@gmail.com>
2022-08-24 15:22:03 +0530

    new

---
+++ PracticeSet05.java/Set5_Pr_11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Set5_Pr_11 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = s.nextInt();
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            sum += i*2;
+        }
+        System.out.printf("Sum of first %d even numbers = %d", n, sum);
+    }
+}
