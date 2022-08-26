6338245ed79d53f896c06c0f459ce0b90de31c1e
Anurup <anurupkoli01@gmail.com>
2022-08-26 23:40:18 +0530

    new

---
+++ PracticeSet06.java/Set6_Pr_09.java
@@ -0,0 +1,17 @@
+public class Set6_Pr_09 {
+    public static void main(String[] args) {
+        int []array = {1,2,3,4,5,6};
+        int n = array.length;
+        int length = array.length/2;
+        int temp;
+        for(int i=0; i<length; i++){
+            temp = array[i];
+            array[i] = array[n-1-i];
+            array[n-1-i] = temp;
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.print(array[i] + " ");
+        }
+    }
+}
